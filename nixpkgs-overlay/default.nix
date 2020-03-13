bc20b5d0f3a85e00ddd4d32a3e7e58c7b2e00482
Emery Hemingway <ehmry@posteo.net>
2020-03-13 23:32:15 +0530

    Add gubbi-font

--- nixpkgs-overlay/default.nix
+++ nixpkgs-overlay/default.nix
@@ -1,4 +1,5 @@
 self: super: {
   deltachat-electron = self.callPackage ./deltachat-electron { };
+  gubbi-font = self.callPackage ./gubbi-font { };
   navilu-font = self.callPackage ./navilu-font { };
 }
