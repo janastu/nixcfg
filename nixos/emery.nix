27dc08d18e6e54137fe5b500ddc2f7b0f7f7d86a
Emery Hemingway <ehmry@posteo.net>
2020-03-27 19:32:14 +0530

    nixos: add emery submodule

---
+++ nixos/emery.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+let
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgL2kRs+cXAcUzOO2Tp+mtMBVuHqMuslQy3LN+HLSP4 emery@nixos"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDnFa8p+KnOPSSqdPr6yvFcULgSM/QHzEO8TradwiW59 emergency"
+  ];
+in {
+
+  networking.firewall.extraCommands =
+    "ip6tables -A INPUT -s 200:3b0f:84ca:1041:eca6:15f8:451d:c1fe -j ACCEPT";
+
+  programs = {
+    fish.enable = true;
+    tmux = {
+      enable = true;
+      secureSocket = true;
+    };
+  };
+
+  users.users.emery = {
+    isNormalUser = true;
+    description = "Emery";
+    extraGroups = [ "users" "wheel" ];
+    hashedPassword =
+      "$6$ZgiLSFCQPW0DB0i$aPeZ9E62y2OvqRbNAEL.8IK30YgvyLy6UOitN6A.li.YmUrmYAh1ukB844MFp3KlTpYzi5e80hRIg1Vx1F0uO/";
+    uid = 1000;
+    openssh.authorizedKeys = { inherit keys; };
+    shell = pkgs.fish;
+  };
+
+  users.users.root.openssh = {
+    inherit (config.users.users.emery.openssh) authorizedKeys;
+  };
+
+}
