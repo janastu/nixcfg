612e45cbb2534fcf4615a9aa2093cfc94fe951d0
Emery Hemingway <ehmry@posteo.net>
2020-03-27 19:31:43 +0530

    nixos: add janastu.workstation option

--- nixos/default.nix
+++ nixos/default.nix
@@ -2,26 +2,59 @@
 #
 
 { config, pkgs, lib, ... }:
+with lib;
 
 {
-  fonts = {
-    enableDefaultFonts = true;
-    fonts = with pkgs;
-      let lohit = with builtins; filter isAttrs (attrValues lohit-fonts);
-      in lohit ++ [ gubbi-font navilu-font ];
-    # Install Kannada fonts
+  # Add Janastu options
+
+  options.janastu.workstation = {
+    enable = mkEnableOption "Whether is a workstation machine";
   };
 
-  i18n.defaultLocale = lib.mkDefault "en_IN.UTF-8";
+  # Apply Janastu configuration
 
-  nix.gc.automatic = lib.mkDefault true;
-  # Make regular garbage collections of /nix/store,
-  # this is a reasonable default unless you are hacking
-  # on packages.
+  config = let
+    cfg = config.janastu;
+    mkIfWorkstation = lib.mkIf cfg.workstation.enable;
+  in {
 
-  nixpkgs.overlays = [ (import ../nixpkgs-overlay) ];
-  # Add some extra packages not in the NixOS release
+    fonts = mkIfWorkstation {
+      enableDefaultFonts = true;
+      fonts = with pkgs;
+        let lohit = with builtins; filter isAttrs (attrValues lohit-fonts);
+        in lohit ++ [ gubbi-font navilu-font ];
+      # Install Kannada fonts
+    };
 
-  time.timeZone = lib.mkDefault "Asia/Kolkata";
-  # Override the default time zone
+    networking.extraHosts = ''
+      205:fe6c:567e:cb71:e831:68ae:4a95:fd2e jitsi.janastu.y
+      205:fe6c:567e:cb71:e831:68ae:4a95:fd2e nextcloud.janastu.y
+    '';
+
+    nix.gc.automatic = lib.mkDefault true;
+    # Make regular garbage collections of /nix/store,
+    # this is a reasonable default unless you are hacking
+    # on packages.
+
+    nixpkgs.overlays = [ (import ../nixpkgs-overlay) ];
+    # Add some extra packages not in the NixOS release
+
+    i18n.defaultLocale = lib.mkDefault "en_IN.UTF-8";
+
+    time.timeZone = lib.mkDefault "Asia/Kolkata";
+    # Override the default time zone
+
+    services.avahi = mkDefault {
+      # Enable mDNS (.local resolution)
+      enable = true;
+      nssmdns = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        workstation = cfg.workstation.enable;
+      };
+    };
+
+  };
+
 }
