6588c83b60f879b91494c11e21b88bcfff3af1d9
Emery Hemingway <ehmry@posteo.net>
2020-03-31 19:46:10 +0530

    thinkcentre: redirect http://mumble.janastu.org

--- hosts/thinkcentre/murmur.nix
+++ hosts/thinkcentre/murmur.nix
@@ -1,13 +1,18 @@
 { config, ... }:
 
-{
+let fqdn = "mumble.janastu.org";
+in {
   services.murmur = {
     enable = true;
     bonjour = true;
-    # registerHostname = "talk.janastu.org.";
-    # registerName = "janastu";
+    registerHostname = fqdn;
+    registerName = "janastu";
   };
+
+  services.nginx.enable = true;
+  services.nginx.virtualHosts.${fqdn} = { globalRedirect = "www.mumble.info"; };
 
   networking.firewall.allowedTCPPorts = [ config.services.murmur.port ];
   networking.firewall.allowedUDPPorts = [ config.services.murmur.port ];
 }
+
