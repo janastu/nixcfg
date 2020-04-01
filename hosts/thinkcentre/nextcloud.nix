a62019e004b8f09e5b449bee30113418c2fb8316
Emery Hemingway <ehmry@posteo.net>
2020-04-01 11:46:12 +0530

    thinkcentre: make nextcloud the default vhost

--- hosts/thinkcentre/nextcloud.nix
+++ hosts/thinkcentre/nextcloud.nix
@@ -1,9 +1,10 @@
 { pkgs, lib, ... }:
 
-{
+let fqdn = "nextcloud.janastu.org";
+in {
   services.nextcloud = {
     enable = true;
-    hostName = "nextcloud.janastu.org";
+    hostName = fqdn;
     nginx.enable = true;
     config.adminuser = "admin";
     config.adminpassFile = "/root/nextcloud.pass";
@@ -11,9 +12,10 @@
       [ "192.168.1.2" "122.166.208.140" ];
   };
 
-  services.nginx.virtualHosts."nextcloud.janastu.org" = {
+  services.nginx.virtualHosts.${fqdn} = {
     addSSL = true;
     enableACME = true;
+    default = true;
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
